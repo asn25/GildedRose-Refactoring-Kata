41b240ed3040c6b7c3dae02dec15b8c6c534dad7
asn25 <asn22111@gmail.com>
2024-12-11 14:24:01 +0200

    Adjust ensureQualityInsideLimits() logic of AbstractItemQualityManager
    Issue: Refactor-update-quality-method
    by: asn

--- Java/src/main/java/com/gildedrose/businessrules/item/quality/AbstractItemQualityManager.java
+++ Java/src/main/java/com/gildedrose/businessrules/item/quality/AbstractItemQualityManager.java
@@ -15,14 +15,24 @@ public abstract class AbstractItemQualityManager implements ItemQualityManager  
     // quality can't be higher than 50;
     // (with exception for "Sulfuras" with Quality=80 that is handled by SulfurasQualityManager class)
     protected int getIncrementedQualityWithLimit(int quality, int increment) {
-        return Math.min(quality + increment, QUALITY_LIMIT_MAX);
+        return ensureQualityInsideLimits(quality + increment);
+
     }
 
 
     // get decremented quality;
     // quality can't be lower than 0
     protected int getDecrementedQualityWithLimit(int quality, int decrement) {
-        return Math.max(quality - decrement, QUALITY_LIMIT_MIN);
+        return ensureQualityInsideLimits(quality - decrement);
+    }
+
+
+    private int ensureQualityInsideLimits(int quality) {
+        if (quality < QUALITY_LIMIT_MIN)
+            return QUALITY_LIMIT_MIN;
+        else if (quality > QUALITY_LIMIT_MAX)
+            return QUALITY_LIMIT_MAX;
+        return quality;
     }
 
 }
